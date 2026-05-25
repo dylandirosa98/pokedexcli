472717d78eb65c9961e98891a85bfd1f8e96a3b8
dylandirosa98 <dylandirosa980@gmail.com>
2026-05-25 15:25:42 -0400

    all files from first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dylandirosa98/pokedexcli
+
+go 1.26.3
---
+++ repl.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	newText := strings.ToLower(text)
+	words := strings.Fields(newText)
+	return words
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    exit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays help mesage",
+			callback:    help,
+		},
+	}
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := cleanInput(scanner.Text())
+		if len(text) == 0 {
+			continue
+		}
+		if commands[text[0]].name == "" {
+			print("Unknown command")
+		} else {
+			commands[text[0]].callback()
+		}
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func exit() error {
+	print("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return fmt.Errorf("exiting the program")
+}
+
+func help() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	commands := getCommands()
+	for _, i := range commands {
+		fmt.Printf("%s: %s\n", i.name, i.description)
+	}
+	return fmt.Errorf("you have been helped")
+}
